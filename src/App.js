7c4f07d7f4d93dc1000a1cdc85bda815808fd44f
Bilal Choudhary <bilal3512@gmail.com>
2025-07-19 20:04:23 +0530

    feat: Add Vercel Speed Insights

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { notesData } from './notes-data.js'; // Import notes from the local file
 
 // --- Helper Components ---
@@ -94,6 +95,7 @@ export default function App() {
             )}
             <Footer />
             <Analytics />
+            <SpeedInsights />
         </div>
     );
 }
